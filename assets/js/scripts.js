0c52095bc17081ca8cab887f307ebe1e400610e8
Rafael <viper_rafa_@hotmail.com>
2019-01-28 23:02:00 -0600

    Modifications

---
+++ assets/js/scripts.js
@@ -0,0 +1,8 @@
+\\
+const elements = document.querySelectorAll('[data-chaffle]');
+Array.prototype.forEach.call(elements, function (el) {
+  const chaffle = new Chaffle(el, { /* options */ });
+  el.addEventListener('mouseover', function () {
+    chaffle.init();
+  });
+});
